5f46449317397ea3ca72dd73974ecf37ab3296b9
wzt3309 <531946934@qq.com>
2017-04-17 10:46:18 +0800

    java编程思想 并发，javase的并发工具与wait/notify

---
+++ src/main/java/tij/wzt/u21concurrent/DelayQueueDemo.java
@@ -0,0 +1,88 @@
+package tij.wzt.u21concurrent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 不实现Runnable都行，因为直接通过一个线程从DelayQueue中take任务，调用任务run()方法来执行
+ * 而非使用启动器或者线程方式
+ * @author wzt
+ *
+ */
+class DelayedTask implements Runnable, Delayed {
+	private static int counter = 0;
+	protected static List<DelayedTask> delayedTasks = new ArrayList<>();
+	private final int id = ++counter;
+	private final int delay;
+	private final long trigger;
+	public DelayedTask(int delay) {
+		this.delay = delay;
+		trigger = System.nanoTime() + 
+				TimeUnit.NANOSECONDS.convert(delay, 
+						TimeUnit.MILLISECONDS);
+		delayedTasks.add(this);
+	}
+	@Override
+	public int compareTo(Delayed o) {
+		DelayedTask other = (DelayedTask) o;
+		if(this.trigger < other.trigger) return -1;
+		if(this.trigger > other.trigger) return 1;
+		return 0;
+	}
+	/**
+	 * 计算还差多久到期
+	 */
+	@Override
+	public long getDelay(TimeUnit unit) {
+		return unit.convert(System.nanoTime() - this.trigger, 
+				TimeUnit.NANOSECONDS);
+	}
+
+	@Override
+	public void run() {
+		System.out.println(this + " ");
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Run([%-4d]-task[%d])", delay, id);
+	}
+}
+public class DelayQueueDemo {
+	public static void main(String[] args) {
+		// JKD1.8K可以不加final语法糖
+		final DelayQueue<DelayedTask> queue = new DelayQueue<>();
+		final ExecutorService exec =Executors.newCachedThreadPool();
+		Random random = new Random();
+		for(int i = 0;i < 10;i++) {
+			queue.put(new DelayedTask(random.nextInt(5000)));
+		}
+		// 添加最后一个任务来结束启动器
+		queue.put(new DelayedTask(5000) {
+			@Override
+			public void run() {
+				System.out.println("queue is over");
+				exec.shutdownNow();
+			}
+		});	
+		// 用一个线程按优先队列顺序运行
+		exec.execute(new Runnable() {			
+			@Override
+			public void run() {
+				while(!Thread.interrupted()) {
+					try {
+						queue.take().run();
+					} catch (InterruptedException e) {						
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+	}
+}
---
+++ src/main/java/tij/wzt/u21concurrent/Restaurant.java
@@ -0,0 +1,98 @@
+package tij.wzt.u21concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Meal {
+	private final int orderNum;
+	public Meal(int orderNum) { this.orderNum = orderNum; }
+	@Override
+	public String toString() {
+		return "Meal(" + orderNum + ")";
+	}
+}
+class WaitPerson implements Runnable {
+	private final Restaurant restaurant;
+	public WaitPerson(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while(!Thread.interrupted()) {
+				synchronized (this) {
+					while(restaurant.meal == null) {
+						wait();
+					}
+				}
+				System.out.println("WaitPerson got " + restaurant.meal);
+				synchronized (restaurant.chief) {
+					restaurant.meal = null;
+					restaurant.chief.notify();
+				}
+			}
+		} catch (InterruptedException e) {
+			System.out.println("WaitPerson is Interrupted");
+		}
+	}	
+}
+class Chief implements Runnable {
+	private final Restaurant restaurant;
+	private int count = 0;
+	public Chief(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+	@Override
+	public void run() {
+		try {
+			while(!Thread.interrupted()) {
+				synchronized (this) {
+					while(restaurant.meal != null) {
+						wait();
+					}
+				}
+				if(++count > 10) {
+					System.out.println("Out of order");
+					restaurant.exec.shutdownNow();
+				}
+				System.out.print("order up ");
+				synchronized (restaurant.waitPerson) {
+					restaurant.meal = new Meal(count);
+					restaurant.waitPerson.notify();
+				}
+				synchronized (restaurant) {
+					restaurant.notify();
+				}
+				TimeUnit.MILLISECONDS.sleep(200);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Chief is interrupted");
+		}
+	}
+}
+public class Restaurant {
+	Meal meal;
+	WaitPerson waitPerson = new WaitPerson(this);
+	Chief chief = new Chief(this);
+	ExecutorService exec = Executors.newCachedThreadPool();
+	public Restaurant() {
+		exec.execute(chief);
+		exec.execute(waitPerson);
+	}
+	public static void main(String[] args) {
+		Restaurant r = new Restaurant();
+		try {
+			synchronized (r) {
+				while(!r.exec.isShutdown()) {
+					r.wait();
+				}
+			}
+		} catch (InterruptedException ignore) {
+		}
+		System.out.println(r.meal);
+		
+		
+	}
+}
