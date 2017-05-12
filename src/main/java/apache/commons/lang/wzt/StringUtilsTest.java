cc9610b4d927702779ed3f380572560efa969f13
wzt3309 <531946934@qq.com>
2017-05-12 23:00:49 +0800

    添加idea支持

---
+++ src/main/java/apache/commons/lang/wzt/StringUtilsTest.java
@@ -0,0 +1,14 @@
+package apache.commons.lang.wzt;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+public class StringUtilsTest {
+	
+	@Test
+	public void testStrBlank() {
+		System.out.println(StringUtils.isBlank(" \r\n"));
+		System.out.println(StringUtils.isAnyBlank(" ", "bar"));
+	}
+}
